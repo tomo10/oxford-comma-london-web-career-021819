68631231ca01e54ba4d68da9e2402cee5e90fc16
Thomas Edwards <thce28@gmail.com>
2019-02-08 16:05:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+    
+  elsif
+  array.length == 2
+  array.join(" and ")
+  
+  elsif
+  array.length >= 3
+  last = array.pop
+  string = array.join(", ")
+  string + ", and " + last 
+  
 end
+end 
